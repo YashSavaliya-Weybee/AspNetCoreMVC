c32185778a355c219103fdc822546ffbdede68b9
Yash Savaliya <Yashsavaliya@webtheuniverse.com>
2022-09-08 11:15:28 +0530

    IOptionMoniter

--- BookStore/BookStore/Controllers/HomeController.cs
+++ BookStore/BookStore/Controllers/HomeController.cs
@@ -14,19 +14,23 @@ namespace BookStore.Controllers
     public class HomeController : Controller
     {
         private readonly NewBookAlertConfig _newBookAlertConfiguration;
+        private readonly NewBookAlertConfig _thirdPartyBookConfiguration;
         private readonly IMessageRepository _messageRepository;
 
         public HomeController(IOptionsSnapshot<NewBookAlertConfig> newBookAlertConfiguration, IMessageRepository messageRepository)
         {
-            _newBookAlertConfiguration = newBookAlertConfiguration.Value;
+            _newBookAlertConfiguration = newBookAlertConfiguration.Get("InternalBook");
+            _thirdPartyBookConfiguration = newBookAlertConfiguration.Get("ThirdPartyBook");
             _messageRepository = messageRepository;
         }
 
         public ViewResult Index()
         {
             bool isDisplay = _newBookAlertConfiguration.DisplayNewBookAlert;
+            bool isDisplay1 = _thirdPartyBookConfiguration.DisplayNewBookAlert;
 
-            var value = _messageRepository.GetName();
+            //IOptionMoniter------------------
+            //var value = _messageRepository.GetName();
 
             return View();
         }
